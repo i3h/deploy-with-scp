3322538ea7ef046c68bcbbc12d7d6298e4481e25
noobly314 <noobly314@gmail.com>
2020-04-06 14:19:19 +0800

    finish

---
+++ deploy-with-scp.js
@@ -0,0 +1,50 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const fs = require('fs').promises;
+const { exec } = require('child_process');
+
+async function run() {
+  try {
+    const src = core.getInput('src');
+    const dest = core.getInput('dest');
+    const username = core.getInput('username');
+    const server_ip = core.getInput('server-ip');
+    const ssh_key = core.getInput('ssh-key');
+    const proxy_username = core.getInput('proxy-username');
+    const proxy_server_ip = core.getInput('proxy-server-ip');
+    const proxy_ssh_key = core.getInput('proxy-ssh-key');
+
+    // Check proxy input
+    let hasProxy = false;
+    if (proxy_ssh_key != '') {
+      hasProxy = true;
+    }
+
+    // mkdir
+    await fs.mkdir('.ssh', { mode: '700' });
+
+    // Write key to file
+    await fs.writeFile('.ssh/ssh_key', ssh_key, { mode: '600' });
+    if (hasProxy) {
+      await fs.writeFile('.ssh/proxy_ssh_key', proxy_ssh_key, { mode: '600' });
+    }
+
+    // scp src root@server_ip:dest
+    let cmd;
+    if (!hasProxy) {
+      cmd = `scp -r -o StrictHostKeyChecking=no -i .ssh/ssh_key ${src} ${username}@${server_ip}:${dest}`;
+    } else {
+      cmd = `scp -r -o StrictHostKeyChecking=no -i .ssh/ssh_key -o ProxyCommand='ssh -W %h:%p -o StrictHostKeyChecking=no -i .ssh/proxy_ssh_key ${proxy_username}@${proxy_server_ip}' ${src} ${username}@${server_ip}:${dest}`;
+    }
+    console.log(cmd);
+    exec(cmd, (error, stdout, stderr) => {
+      if (error) {
+        console.log(error);
+      }
+    });
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+module.exports = run;
---
+++ main.js
@@ -0,0 +1,3 @@
+const run = require('./deploy-with-scp');
+
+run();
